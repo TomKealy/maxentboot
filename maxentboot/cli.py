fd030d981451d0a203f2b2b8005cd145e8940966
TomKealy <tom.kealy.tk@gmail.com>
2025-01-26 20:04:45 +0100

    update names to make tests pass

--- maxentboot/cli.py
+++ maxentboot/cli.py
@@ -6,8 +6,8 @@ import click
 @click.command()
 def main():
     """Main entrypoint."""
-    click.echo("meboot")
-    click.echo("=" * len("meboot"))
+    click.echo("maxentboot")
+    click.echo("=" * len("maxentboot"))
     click.echo("Maximum Entropy Bootstrap for timeseries")
 
 
